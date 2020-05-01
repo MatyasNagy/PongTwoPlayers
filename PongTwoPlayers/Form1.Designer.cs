df020c6ad9be2abe63f01db4543ed7c1ef93310b
matyas.nagy <matyas.nagy@gmail.com>
2020-05-01 18:56:23 +0200

    Add project files.

---
+++ PongTwoPlayers/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace PongTwoPlayers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PlayGround = new System.Windows.Forms.Panel();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.GameOverLabel = new System.Windows.Forms.Label();
+            this.RacketLeft = new System.Windows.Forms.PictureBox();
+            this.RacketRight = new System.Windows.Forms.PictureBox();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.PlayGround.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.RacketLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RacketRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PlayGround
+            // 
+            this.PlayGround.Controls.Add(this.scoreLabel);
+            this.PlayGround.Controls.Add(this.GameOverLabel);
+            this.PlayGround.Controls.Add(this.RacketLeft);
+            this.PlayGround.Controls.Add(this.RacketRight);
+            this.PlayGround.Controls.Add(this.Ball);
+            this.PlayGround.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PlayGround.Font = new System.Drawing.Font("Comic Sans MS", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayGround.Location = new System.Drawing.Point(0, 0);
+            this.PlayGround.Name = "PlayGround";
+            this.PlayGround.Size = new System.Drawing.Size(1200, 600);
+            this.PlayGround.TabIndex = 0;
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.ForeColor = System.Drawing.Color.White;
+            this.scoreLabel.Location = new System.Drawing.Point(0, 0);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(40, 45);
+            this.scoreLabel.TabIndex = 4;
+            this.scoreLabel.Text = "0";
+            // 
+            // GameOverLabel
+            // 
+            this.GameOverLabel.AutoSize = true;
+            this.GameOverLabel.ForeColor = System.Drawing.Color.Red;
+            this.GameOverLabel.Location = new System.Drawing.Point(0, 0);
+            this.GameOverLabel.Name = "GameOverLabel";
+            this.GameOverLabel.Size = new System.Drawing.Size(186, 45);
+            this.GameOverLabel.TabIndex = 3;
+            this.GameOverLabel.Text = "Game Over";
+            this.GameOverLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // RacketLeft
+            // 
+            this.RacketLeft.BackColor = System.Drawing.Color.White;
+            this.RacketLeft.Location = new System.Drawing.Point(82, 132);
+            this.RacketLeft.Name = "RacketLeft";
+            this.RacketLeft.Size = new System.Drawing.Size(20, 100);
+            this.RacketLeft.TabIndex = 2;
+            this.RacketLeft.TabStop = false;
+            // 
+            // RacketRight
+            // 
+            this.RacketRight.BackColor = System.Drawing.Color.White;
+            this.RacketRight.Location = new System.Drawing.Point(713, 132);
+            this.RacketRight.Name = "RacketRight";
+            this.RacketRight.Size = new System.Drawing.Size(20, 100);
+            this.RacketRight.TabIndex = 1;
+            this.RacketRight.TabStop = false;
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.Color.Red;
+            this.Ball.Location = new System.Drawing.Point(399, 116);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(20, 20);
+            this.Ball.TabIndex = 0;
+            this.Ball.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowText;
+            this.ClientSize = new System.Drawing.Size(1200, 600);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PlayGround);
+            this.Name = "Form1";
+            this.Text = "Pong 1v1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.PlayGround.ResumeLayout(false);
+            this.PlayGround.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.RacketLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.RacketRight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PlayGround;
+        private System.Windows.Forms.PictureBox RacketLeft;
+        private System.Windows.Forms.PictureBox RacketRight;
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label GameOverLabel;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PongTwoPlayers/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PongTwoPlayers
+{
+    public partial class Form1 : Form
+    {
+
+        int BallSpeed = 5;
+        int score = 0;
+
+        bool racketLeftUP= false;
+        bool racketLeftDOWN = false;
+        bool racketRightUP = false;
+        bool racketRightDOWN = false;
+        
+
+        //Balls direction on the PlayGround
+        bool ballDOWN = true;
+        bool ballRIGHT = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Cursor.Hide();
+            FormBorderStyle = FormBorderStyle.None;
+            TopMost = true;
+            Bounds = Screen.PrimaryScreen.Bounds;
+
+            ResetGame();
+            
+        }
+
+        private void ResetGame()
+        {
+            GameOverLabel.Visible = false;
+            RacketLeft.Left = PlayGround.Left + 20;
+            RacketLeft.Top = PlayGround.Bottom / 2-RacketLeft.Height/2;
+            RacketRight.Left = PlayGround.Width - (20+RacketRight.Width);
+            RacketRight.Top = PlayGround.Bottom / 2-RacketRight.Height/2;
+            BallStartPosition();
+            timer.Enabled = true;
+            score = 0;
+            Scoring(0);
+
+        }
+
+        private void BallStartPosition()
+        {
+            Random r = new Random();
+            Ball.Left = PlayGround.Width / 2;
+            Ball.Top = r.Next(0, PlayGround.Bottom);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //
+            // To much keys down at a sametime, it must recognize for the pressed buttons
+            //
+
+
+            if (e.KeyCode == Keys.Escape) { Close(); }
+            if (e.KeyCode == Keys.F1) { ResetGame(); }
+
+            if (e.KeyCode == Keys.Q) { racketLeftUP = true; }
+            if (e.KeyCode == Keys.A) { racketLeftDOWN = true; }
+            if (e.KeyCode == Keys.O) { racketRightUP = true; }
+            if (e.KeyCode == Keys.L) { racketRightDOWN= true; }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Q) { racketLeftUP = false; }
+            if (e.KeyCode == Keys.A) { racketLeftDOWN = false; }
+            if (e.KeyCode == Keys.O) { racketRightUP = false; }
+            if (e.KeyCode == Keys.L) { racketRightDOWN = false; }
+        }
+
+        private void RocketsMove()
+        {
+            //
+            // Separeted Method this move the rackets, not problem with multiple keys down
+            //
+
+            if (racketLeftUP) { RacketLeft.Top -= 10; }
+            if (racketLeftDOWN) { RacketLeft.Top += 10; }
+            if (racketRightUP) { RacketRight.Top -= 10; }
+            if (racketRightDOWN) { RacketRight.Top += 10; }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            timer.Interval = 10;
+            RocketsMove();
+            BallMove();
+        }
+
+
+        private void BallMove()
+        {
+            //From ball position -> To ball direction
+
+            if (Ball.Top <= PlayGround.Top) { ballDOWN = true; }
+            if (Ball.Bottom >= PlayGround.Bottom) { ballDOWN = false; }
+
+            if (Ball.Left >= RacketLeft.Right && Ball.Right >= RacketRight.Left && Ball.Top>= RacketRight.Top &&Ball.Bottom<=RacketRight.Bottom) { ballRIGHT = false; Scoring(1); }
+            if (Ball.Right > RacketRight.Right) { GameOver(2); }
+            if (Ball.Left<=RacketLeft.Right && Ball.Right <= RacketRight.Left&& Ball.Top >= RacketLeft.Top && Ball.Bottom <= RacketLeft.Bottom) { ballRIGHT = true; Scoring(1); }
+            if (Ball.Left<RacketLeft.Left) { GameOver(1); }
+
+            //Ball move to a chosed direction.
+            if      (ballDOWN) { Ball.Top += BallSpeed; }
+            else    { Ball.Top -= BallSpeed; }
+            if      (ballRIGHT) { Ball.Left += BallSpeed; }
+            else    { Ball.Left -= BallSpeed; }
+
+            
+
+        }
+
+        private void GameOver(int loser_playerID)
+        {
+            if (loser_playerID == 2) { GameOverLabel.Text = "GAME OVER\nWinner: Player1\nScore: "+ score.ToString()+"\nRestart: F1\nExit: Esc"; }
+            if (loser_playerID == 1) { GameOverLabel.Text = "GAME OVER\nWinner: Player2\nScore: "+score.ToString()+"\nRestart: F1\nExit: Esc"; }
+
+            GameOverLabel.Left = (PlayGround.Width / 2) - (GameOverLabel.Width / 2);
+            GameOverLabel.Top= (PlayGround.Height / 2) - (GameOverLabel.Height / 2);
+            GameOverLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            GameOverLabel.Visible=true;
+            timer.Enabled = false;
+        }
+
+        private void Scoring(int sco)
+        {
+            score += sco;
+            scoreLabel.Text = "Score: "+score.ToString();
+            scoreLabel.Left = (PlayGround.Width / 2)-(scoreLabel.Width/2);
+        }
+    }
+}
